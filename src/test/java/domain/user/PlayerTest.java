1c8ab5a30e4129528a7665b52210c58274468670
jaeyoungjang2 <lion85488@gmail.com>
2021-12-04 19:06:29 +0900

    refactor: 테스트 메서드 이름 변경  - 플레이어 이름 길이에 관한 테스트 이므로 playerNumberTest -> playerNameLengthTest 로 변경

--- src/test/java/domain/user/PlayerTest.java
+++ src/test/java/domain/user/PlayerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class PlayerTest {
     @DisplayName("플레이어 이름이 5자 초과인 경우, 예외 사항 확인")
     @Test
-    void playerNumberTest() {
+    void playerNameLengthTest() {
         Assertions.assertThatThrownBy(() -> {
             new Player("123456", 10000);
         }).hasMessageContaining("플레이어의 이름은 5자 이하로 구성되어야 합니다.");
