232387e93ce58fd8bfdf61c779a07479f19b0ca3
jaeyoungjang2 <lion85488@gmail.com>
2021-12-15 15:04:29 +0900

    feat: player bettingMoney 받아오는 로직 구현

--- src/main/java/controller/BlackJackController.java
+++ src/main/java/controller/BlackJackController.java
@@ -1,10 +1,21 @@
 package controller;
 
+import domain.user.Player;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import view.InputView;
 
 public class BlackJackController {
 
+    private List<Player> players = new ArrayList<>();
+
     public void run() {
         String stringPlayer = InputView.requirePlayer();
+        List<String> players = Arrays.asList(stringPlayer.split(","));
+        for (String player : players) {
+            String bettingMoney = InputView.requireBettingMoney(player);
+            this.players.add(new Player(player, Integer.parseInt(bettingMoney)));
+        }
     }
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,4 +10,9 @@ public class InputView {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요. (쉼표 기준으로 분리)");
         return sc.nextLine();
     }
+
+    public static String requireBettingMoney(String player) {
+        System.out.printf("%s의 베팅 금액은?", player);
+        return sc.nextLine();
+    }
 }
